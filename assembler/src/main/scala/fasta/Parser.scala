b2b431a029f9ddf1f7e9371e759e27753436415c
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2020-04-02 16:52:59 +0300

    feat: Add fasta support

---
+++ assembler/src/main/scala/fasta/Parser.scala
@@ -0,0 +1,8 @@
+package fasta
+
+trait Parser[T] {
+  def show(record: T): Seq[String]
+  def shows(records: Seq[T]): Seq[String]
+  def read(line: Seq[String]): Either[String, T]
+  def reads(lines: Seq[String]): Either[String, Seq[T]]
+}
---
+++ assembler/src/main/scala/fasta/Record.scala
@@ -0,0 +1,3 @@
+package fasta
+
+case class Record(description: String, data: String)
