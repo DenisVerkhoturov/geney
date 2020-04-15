1f2889a4e5ed15ce1d580c74ba484025d7fa6ba4
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-04-15 15:03:34 +0300

    chore: add silencer library

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,13 @@ object Dependencies {
   private object Version {
     val scalastic = "3.1.1"
     val scalatest = "3.1.1"
+    val silencer  = "1.6.0"
   }
 
-  val scalastic = "org.scalactic" %% "scalactic" % Version.scalastic % Test
-  val scalatest = "org.scalatest" %% "scalatest" % Version.scalatest % Test
+  val scalastic   = "org.scalactic"   %% "scalactic"   % Version.scalastic % Test
+  val scalatest   = "org.scalatest"   %% "scalatest"   % Version.scalatest % Test
+  val silencerLib = "com.github.ghik" % "silencer-lib" % Version.silencer  % Provided cross CrossVersion.full
+  val silencerPlugin = compilerPlugin(
+    "com.github.ghik" % "silencer-plugin" % Version.silencer % Provided cross CrossVersion.full
+  )
 }
