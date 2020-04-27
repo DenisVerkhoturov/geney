687589f281abe1075e4fdc3ee28b84481b3ea095
Sergey Kozhin <kozhin.sv@edu.spbstu.ru>
2020-04-27 19:39:09 +0300

    feat: add path finding to graph

--- assembler/src/main/scala/graph/DeBruijnGraph.scala
+++ assembler/src/main/scala/graph/DeBruijnGraph.scala
@@ -5,6 +5,27 @@ class DeBruijnGraph(reads: LazyList[String], k: Int) {
 
   val (nodes, branches) = buildGraph(reads flatMap kmers, Set(), Map() withDefaultValue List())
 
+  lazy val path: String = {
+    def findPath(acc: String, branches: Map[String, List[String]]): String =
+      if (branches.isEmpty) {
+        if ((acc take k - 1) == (acc takeRight k - 1)) acc dropRight k - 1
+        else ""
+      } else {
+        val left = acc takeRight k - 1
+        val paths = for {
+          right <- branches(left).to(LazyList)
+          path = findPath(acc + right.last, deleteBranch(branches, (left, right)))
+          if path.nonEmpty
+        } yield path
+
+        if (paths.isEmpty) ""
+        else paths.head
+      }
+
+    if (nodes.nonEmpty) findPath(nodes.head, branches)
+    else ""
+  }
+
   private def kmers(read: String): LazyList[String] =
     if (read.length < k) LazyList.empty
     else (read take k) #:: kmers(read.tail)
@@ -20,4 +41,12 @@ class DeBruijnGraph(reads: LazyList[String], k: Int) {
         buildGraph(rest, nodes incl left incl right, branches updated (left, right :: branches(left)))
       }
     }
+
+  private def deleteBranch(branches: Map[String, List[String]], branch: (String, String)) = {
+    val (left, right) = branch
+    val leftBranches  = branches(left)
+
+    if (leftBranches.size == 1) branches - left
+    else branches updated (left, leftBranches diff List(right))
+  }
 }
