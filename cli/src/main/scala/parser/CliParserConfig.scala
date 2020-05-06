b0d214e7cdc6fff5093f00ce6e4b4daebfe1e731
Александра Прохорова <adlovlac@gmail.com>
2020-05-06 10:59:09 +0300

    feat: Clue CLI interface and assembly
    * fixed error with parsing args
    * added check for existence of input file
    * added function for potential processing of graph
    * removed redundant -jar option
    
    Resolves: #15

--- cli/src/main/scala/parser/CliParserConfig.scala
+++ cli/src/main/scala/parser/CliParserConfig.scala
@@ -3,7 +3,6 @@ package parser
 import java.io.File
 
 case class CliParserConfig(
-                   jar: File = new File("."),
                    k: Int = -1,
                    input: File = new File("."),
                    output: File = new File(".")
