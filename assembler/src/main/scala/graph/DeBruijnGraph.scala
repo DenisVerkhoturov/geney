be88a4baf4ade2ee069cc3a4609ea73db1327fc5
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-07-08 14:17:28 +0300

    feat: Add Graph optimization
    Resolves: #17

--- assembler/src/main/scala/graph/DeBruijnGraph.scala
+++ assembler/src/main/scala/graph/DeBruijnGraph.scala
@@ -1,10 +1,6 @@
 package graph
 
-class DeBruijnGraph(reads: LazyList[String], k: Int) {
-  require(k > 1, "k must be more than 1")
-
-  val (nodes, branches) = buildGraph(reads flatMap kmers, Set(), Map() withDefaultValue List())
-
+case class DeBruijnGraph private (nodes: Set[String], branches: Map[String, List[String]], k: Int) {
   lazy val path: String = {
     def findPath(acc: String, branches: Map[String, List[String]]): String =
       if (branches.isEmpty) {
@@ -14,7 +10,7 @@ class DeBruijnGraph(reads: LazyList[String], k: Int) {
         val left = acc takeRight k - 1
         val paths = for {
           right <- branches(left).to(LazyList)
-          path = findPath(acc + right.last, deleteBranch(branches, (left, right)))
+          path = findPath(acc + (right drop k - 2), deleteBranch(branches, (left, right)))
           if path.nonEmpty
         } yield path
 
@@ -26,20 +22,33 @@ class DeBruijnGraph(reads: LazyList[String], k: Int) {
     else ""
   }
 
-  private def kmers(read: String): LazyList[String] =
-    if (read.length < k) LazyList.empty
-    else (read take k) #:: kmers(read.tail)
-
-  private def buildGraph(kmers: LazyList[String],
-                         nodes: Set[String],
-                         branches: Map[String, List[String]]): (Set[String], Map[String, List[String]]) =
-    kmers match {
-      case LazyList() => (nodes, branches)
-      case kmer #:: rest => {
-        val left  = kmer take k - 1
-        val right = kmer takeRight k - 1
-        buildGraph(rest, nodes incl left incl right, branches updated (left, right :: branches(left)))
+  lazy val optimized: DeBruijnGraph = {
+    def optimize(nodes: Set[String], branches: Map[String, List[String]]): (Set[String], Map[String, List[String]]) =
+      branches.find { case (left, rights) => (rights.length == 1) && branches.exists(leadsToNode(left)) } match {
+        case Some((node, List(branch))) =>
+          val newBranch   = node ++ (branch drop k - 2)
+          val newBranches = (branches - node) map replaceNode(newBranch)
+          optimize(nodes - node, newBranches)
+        case None => (nodes, branches)
       }
+
+    val (newNodes, newBranches) = optimize(nodes, branches)
+    new DeBruijnGraph(newNodes, newBranches, k)
+  }
+
+  private def leadsToNode(node: String)(branches: (String, List[String])): Boolean =
+    branches match {
+      case (_, rights) =>
+        rights.exists(_.endsWith(node))
+    }
+
+  private def replaceNode(newBranch: String)(branch: (String, List[String])): (String, List[String]) =
+    branch match {
+      case (left, rights) =>
+        left -> rights.map(branch => {
+          if ((branch takeRight k - 1) == (newBranch take k - 1)) branch ++ (newBranch drop k - 1)
+          else branch
+        })
     }
 
   private def deleteBranch(branches: Map[String, List[String]], branch: (String, String)) = {
@@ -49,4 +58,29 @@ class DeBruijnGraph(reads: LazyList[String], k: Int) {
     if (leftBranches.size == 1) branches - left
     else branches updated (left, leftBranches diff List(right))
   }
+}
+
+object DeBruijnGraph {
+  def apply(reads: LazyList[String], k: Int): DeBruijnGraph = {
+    require(k > 1, "k must be more than 1")
+
+    val (nodes, branches) = buildGraph(reads flatMap kmers(k), Set(), Map() withDefaultValue List(), k: Int)
+    DeBruijnGraph(nodes, branches, k)
+  }
+
+  private def kmers(k: Int)(read: String): LazyList[String] =
+    if (read.length < k) LazyList.empty
+    else (read take k) #:: kmers(k)(read.tail)
+
+  private def buildGraph(kmers: LazyList[String],
+                         nodes: Set[String],
+                         branches: Map[String, List[String]],
+                         k: Int): (Set[String], Map[String, List[String]]) =
+    kmers match {
+      case LazyList() => (nodes, branches)
+      case kmer #:: rest =>
+        val left  = kmer take k - 1
+        val right = kmer takeRight k - 1
+        buildGraph(rest, nodes incl left incl right, branches updated (left, right :: branches(left)), k)
+    }
 }
--- cli/src/main/scala/parser/CliParser.scala
+++ cli/src/main/scala/parser/CliParser.scala
@@ -1,6 +1,5 @@
 package parser
 
-
 import scopt.{OParser, OParserBuilder}
 import java.io.{File, PrintWriter}
 import java.nio.file.Files
@@ -10,7 +9,6 @@ import graph.DeBruijnGraph
 
 import scala.io.Source
 
-
 class CliParser {
   val builder: OParserBuilder[CliParserConfig] = OParser.builder[CliParserConfig]
   val parser: OParser[Unit, CliParserConfig] = {
@@ -18,20 +16,23 @@ class CliParser {
     OParser.sequence(
       programName("CliParser"),
       head("CliParser"),
-      opt[Int]('k',"k")
+      opt[Int]('k', "k")
         .required()
         .action((x, c) => c.copy(k = x))
         .text("k is an  integer property, the length of k that will be used to build De Bruijn graph")
-        .validate( k =>
-          if (k >= 2) success
-          else failure("Argument -k must be >2") ),
+        .validate(
+          k =>
+            if (k >= 2) success
+            else failure("Argument -k must be >2")
+        ),
       opt[File]('i', "input")
         .required()
-        .validate(input =>
-          if (!input.exists()) failure("Input file doesn't exist")
-          else if (input.isDirectory) failure("Path to input file is a directory")
-          else if (!Files.isReadable(input.toPath)) failure("Access to input file is denied")
-          else success
+        .validate(
+          input =>
+            if (!input.exists()) failure("Input file doesn't exist")
+            else if (input.isDirectory) failure("Path to input file is a directory")
+            else if (!Files.isReadable(input.toPath)) failure("Access to input file is denied")
+            else success
         )
         .valueName("<file>")
         .action((x, c) => c.copy(input = x))
@@ -39,26 +40,27 @@ class CliParser {
       opt[File]('o', "output")
         .valueName("<file>")
         .action((x, c) => c.copy(output = x))
-        .validate(output =>
-          if (output.isDirectory) failure("Path to output file is a directory")
-          else if (output.exists()) failure("Output file already exists")
-          else if (!Files.isWritable(output.getParentFile.toPath)) failure("Access to output file is denied")
-          else success
+        .validate(
+          output =>
+            if (output.isDirectory) failure("Path to output file is a directory")
+            else if (output.exists()) failure("Output file already exists")
+            else if (!Files.isWritable(output.getParentFile.toPath)) failure("Access to output file is denied")
+            else success
         )
         .text("output file to write the result to"),
       opt[String]('f', "format")
         .required()
         .action((x, c) => c.copy(format = x))
         .validate {
-          case "fasta" => success
-          case "fastq" => success
+          case "fasta"  => success
+          case "fastq"  => success
           case "derive" => success
-          case _ => failure("Wrong type of format")
+          case _        => failure("Wrong type of format")
         },
-      help('h',"help").text("show usage"),
+      help('h', "help").text("show usage")
     )
   }
-  def parse(args: Array[String]):Unit = {
+  def parse(args: Array[String]): Unit =
     OParser.parse(parser, args, CliParserConfig()) match {
       case Some(config) =>
         println("Args have been parsed successfully.");
@@ -66,10 +68,9 @@ class CliParser {
       case _ =>
         System.err.println("Couldn't parse args.");
     }
-  }
   private def processGraph(config: CliParserConfig): Unit = {
-    val source = Source.fromFile(config.input);
-    val deBruijnGraph = new DeBruijnGraph(LazyList(source.toString()), config.k);
+    val source        = Source.fromFile(config.input);
+    val deBruijnGraph = DeBruijnGraph(LazyList(source.toString()), config.k);
     source.close();
     val writer = new PrintWriter(config.output);
     writer.println("Here could be some info about graph");
@@ -79,7 +80,5 @@ class CliParser {
 object Main {
   val mainParser = new CliParser;
   def main(args: Array[String]): Unit =
-  {
     mainParser.parse(args);
-  }
 }
