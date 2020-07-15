39fbac2a7c74c93178f27656bcdc2d5a8893cbb3
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-07-15 18:58:12 +0300

    feat: force flag for output file force override

--- cli/src/main/scala/parser/CliParserConfig.scala
+++ cli/src/main/scala/parser/CliParserConfig.scala
@@ -3,8 +3,9 @@ package parser
 import java.io.File
 
 case class CliParserConfig(
-                   k: Int = -1,
-                   input: File = new File("."),
-                   output: File = null,
-                   format: String = ""
-                 )
+    k: Int = -1,
+    input: File = new File("."),
+    output: File = null,
+    format: String = "",
+    force: Boolean = false
+)
